2206dcebc62d7c3cdf9ac8869985b9170ef2a356
Sonam <sonam.malhotra2496@gmail.com>
2021-08-18 03:06:21 +0000

    completed all requirements for this code

--- eqArrays.js
+++ eqArrays.js
@@ -9,12 +9,20 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function (array1,array2){
-  if (array1.length === array2.length){
-    return true;
+  if (array1.length === array2.length){ //are the lists of equal size?
+    //yes they are equal in size
+    for(let i =0; i<array1.length;i++) { // are all the items the same
+      if(array1[i]!==array2[i]){
+        return false; //we found one set of items not equal
+      }
+    }
+    return true; //all items are the same
   }
   else{
+    //no they are not equal
     return false;
   }
 }
-
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
