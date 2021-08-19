5513ab2479ef1783eae5cf06e7d97d1990c5ecb5
Sonam <sonam.malhotra2496@gmail.com>
2021-08-19 13:53:33 +0000

    Done

---
+++ eqObjects.js
@@ -0,0 +1,44 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed :[${actual}] !=== [${expected}]`);
+
+  }
+};
+
+const eqObjects = function(Object1, Object2) {
+  if (Object.keys(Object1).length !== Object.keys(Object2).length) {
+    return false;
+
+  } else {
+
+      
+    for (let key of Object.keys(Object1)) {
+       
+      if ((Array.isArray(Object1[key]) && (Object2[key] === false))) {
+        return false;
+      }
+    }
+    for (const key in Object2) {
+   
+      if (!assertEqual(Object1[key], Object2[key])) {
+        return false;
+      }
+
+    }
+    return true;
+  }
+};
+
+let ab = { a: 1, b: "2" };
+let ba = { b: "2", a: 1 };
+console.log(eqObjects(ab, ba)); // => true
+  
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
+
+ab = { a: '1', b: 2 };
+ba = { b: 2, a: '1' };
+console.log(eqObjects(ab, ba));
