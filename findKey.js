2650e322c0926e8e9ca77d8f9203de3cc341b91f
Sonam <sonam.malhotra2496@gmail.com>
2021-08-20 18:24:42 +0000

    implemented findKey

---
+++ findKey.js
@@ -0,0 +1,46 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Paseed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed :[${actual}] !=== [${expected}]`);
+
+  }
+};
+
+const findKey = function(obj,callback){
+  for(let i in obj){
+    // console.log(callback);
+    // console.log(obj[i])
+    if (callback(obj[i])=== true){
+      
+      console.log(i);
+      return obj[i];
+    }else{
+      return undefined;
+    }
+  }
+  
+}
+
+// const findKey = function(object, callback){
+//   for (const i in object) {
+//      // console.log("---", callback(object[i]));
+//    //  console.log("---", object[i]);
+//    if (callback(object[i]) === true){
+//      console.log(i);
+//        return i;
+//    }  
+//   }
+//   return undefined;
+// }
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
