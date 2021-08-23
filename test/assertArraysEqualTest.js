80f29262350ced5c730aede349202871369e632c
Sonam <sonam.malhotra2496@gmail.com>
2021-08-23 19:59:43 +0000

    Done

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,10 @@
+const eqArrays = require('../eqArrays');
+
+const assertArraysEqual = require('../assertArrayEqual');
+const assertEqual = require('../assertEqual');
+
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
