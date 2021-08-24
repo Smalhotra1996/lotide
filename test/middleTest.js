1ea47e42a51ff272c3cd05caf16f7af066a14fb9
Sonam <sonam.malhotra2496@gmail.com>
2021-08-24 00:43:29 +0000

    Done refactoring of middle function using chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,26 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 const assertEqual = require('../assertEqual');
 const middle = require('../middle');
 
 
-console.log(middle([1, 2, 3])); // => [2])
-console.log(middle([1, 2, 3, 4, 5]));// => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
+describe("#middle", () =>{
+  it("retuens 2 for [1,2,3]", ()=> {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns 3 for [1,2,3,4,5]", () => {
+    assert.deepEqual(middle([1,2,3,4,5]), [3]);
+  });
+});
+
+
+
+
+// console.log(middle([1, 2, 3])); // => [2])
+// console.log(middle([1, 2, 3, 4, 5]));// => [3]
+// console.log(middle([1, 2, 3, 4])); // => [2, 3]
+// console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+
 
